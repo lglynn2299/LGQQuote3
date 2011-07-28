25c7121e37d47c9b087e737348e48b3f80b106c5
Leslie Glynn <glynn.leslie@gmail.com>
2011-07-28 19:41:58 -0400

    LG initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LGQQuotes3::Application.config.secret_token = 'da6e8d2ba08f71697944c0d2669289dbc5dbed2b5010d6c212dbe58295fa9364904d640bdb826d659b4b05c900e693d951090e080d36cc800e684b087d7cf594'
