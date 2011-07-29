b2f96512417977fee019f16a8c04eab3e9692468
Leslie Glynn <glynn.leslie@gmail.com>
2011-07-29 11:20:57 -0400

    stylesheet changes

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,9 @@
 class Client < ActiveRecord::Base
+
+has_many :quick_quotes
+
+validates :clientname, :presence => true
+
+validates :clientname, :uniqueness => {:case_sensitive => false}
+
 end
--- app/models/quick_quote.rb
+++ app/models/quick_quote.rb
@@ -1,7 +1,15 @@
 class QuickQuote < ActiveRecord::Base
    belongs_to :user
+   belongs_to :client
+
+ STATUS = %w(Pending Approved Completed)
  
-   validates_presence_of :title, :createdby, :requirements, :client
-   validates_numericality_of :hours, :cost
+   validates :title, :presence => true 
+   validates :requirements, :presence => true
+   validates :user_id, :presence => true
+   validates :client_id, :presence => true
+   #validates_format_of :hours, /[1-9][\d]{0,2}/
+   #validates_format_of :cost, /[1-9][\d]{0,2}/
    validates :title, :length => { :maximum => 50 }
+   validates :status, :inclusion => {:in => STATUS} 
 end
